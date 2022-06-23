93780b359b908accc70587e16c2a8643c0d30cce
phantomramen <ali.hernandez9two@gmail.com>
2022-06-22 20:17:35 -0600

    does it even need to be in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavBar from './components/Nav.js';
 import About from './components/About';
+import Work from './components/PreviousWork';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='app'>
       <NavBar id="nav" />
       <About />
-      
+      <Work />
     </div>
   );
 }
