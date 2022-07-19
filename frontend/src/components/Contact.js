ef615b478247c5a7435df5c31006a2b536652906
phantomramen <ali.hernandez9two@gmail.com>
2022-07-18 21:10:01 -0600

    contact form still

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,19 +1,36 @@
+import React, { useState } from 'react';
 const Contact = (props) => {
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
     return (
         <form>
             <h1>Contact Us Today!</h1>
+
             <label>Name:</label>
-            <input></input>
+            <input
+              placeholder='Name'
+              value={name}>
+              </input>
+            
             <label>Email:</label>
-            <input></input>
+            <input
+              placeholder='Email Address'
+              value={email}>
+              </input>
+            
             <label>Phone:</label>
-            <input></input>
+            <input
+              placeholder='Phone Numer'
+              value={phone}>
+              </input>
+            
             <label>Message:</label>
             <textarea
               type="text"
             //   onChange={handleMessage}
-            //   value={message}
+              value={message}
               placeholder="your message here..."
               name="message"
             />
