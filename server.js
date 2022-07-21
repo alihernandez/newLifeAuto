a98c333d45da754128326bf4bee7f5673ef29bda
phantomramen <ali.hernandez9two@gmail.com>
2022-07-21 16:08:55 -0600

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+app.get('/', (request, response) => {
+    response.status(200).send('This is the home page');
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
