ca4a5964ab10c9b90b2f4f20eece380435f31012
phantomramen <ali.hernandez9two@gmail.com>
2022-07-21 16:12:39 -0600

    Deploy app to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
